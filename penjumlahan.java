9abc20502161e90b020a6a9c8d0c649ec4195d1b
DzakaAlif <dzakaalifnurhakim@smkwikrama.sch.id>
2024-09-19 21:25:55 +0700

    add Jurnal 2

--- penjumlahan.java
+++
@@ -1,8 +0,0 @@
-public class penjumlahan {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println("Hasil penjumlahan: " + c);
-    }
-}
