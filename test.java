5ef919c9d7726c89c0097ab5d430f5e1c1c31941
DzakaAlif <dzakaalifnurhakim@smkwikrama.sch.id>
2024-09-19 21:01:15 +0700

    adding name, nim, and class

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 class helloworld{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Nama: Dzaka Alif Nurhakim");
+        System.out.println("Nim: 607062400023");
+        System.out.println("Kelas: D3IF - 48 - 01");
     }
 }
