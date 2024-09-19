8e4deaf90759bca585400927b6972b251bff120e
DzakaAlif <dzakaalifnurhakim@smkwikrama.sch.id>
2024-09-19 21:10:22 +0700

    first commit

---
+++ penjumlahan.java
@@ -0,0 +1,8 @@
+public class penjumlahan {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("Hasil penjumlahan: " + c);
+    }
+}
