0d00b97fb0de495a21d29127fa7e8474cfaf136d
DzakaAlif <dzakaalifnurhakim@smkwikrama.sch.id>
2024-09-20 13:47:20 +0700

    change name

--- Jurnal-01.java
+++ Jurnal-01.java
@@ -1,6 +1,6 @@
 class helloworld{
     public static void main(String[] args) {
-        System.out.println("Nama: Dzaka Alif Nurhakim");
+        System.out.println("Nama: Dzaki Chrisna Nurhakim");
         System.out.println("Nim: 607062400023");
         System.out.println("Kelas: D3IF - 48 - 01");
     }
